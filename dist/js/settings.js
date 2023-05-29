58a305288801f032e1d568ea9b581fafc4c82a5d
Michal Sak <michusakowicz@gmail.com>
2023-05-29 15:47:08 +0200

    Ready to send pizzeria in Replit

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -122,7 +122,7 @@ export const settings = {
   },
   // CODE ADDED END
   db: {
-    url: 'http://localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     booking: 'bookings',
